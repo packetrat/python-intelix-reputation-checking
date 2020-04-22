0b2ce805454757e464e50d03693a64a0a0634a2f
Sean M Gallagher <packetrat@users.noreply.github.com>
2020-04-22 12:34:22 -0400

    Update intelixiocchecker.py

--- intelixiocchecker.py
+++ intelixiocchecker.py
@@ -4,8 +4,10 @@ clientId = "xxxxxxxxxxxxxxxxxxxx"
 secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 i = intelixclient.client(clientId,secret)
 i2 = intelixclient.client(clientId,secret)
+# enter filename. todo: add parameter passing to accept source list from command line
 inputfile = input("Enter filename containing URIs to be checked: ")  
 f = open( inputfile, 'r')
+# output is both to console and a csv file w/ name based on source file. todo: make output file a CLI parameter
 o = inputfile + '_results.csv'
 opf = open( o, 'w')
 count = 0
