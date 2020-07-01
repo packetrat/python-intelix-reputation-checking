1290859afb650acddb8067ae1dc266851042fb8a
Sean M Gallagher <packetrat@users.noreply.github.com>
2020-07-01 13:56:37 -0400

    Create domainCheck.py

---
+++ domainCheck.py
@@ -0,0 +1,33 @@
+import intelixclient, sys, socket, ipwhois, whois
+#Intelix setup: enter your data here
+clientId = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+i = intelixclient.client(clientId,secret)
+i2 = intelixclient.client(clientId,secret)
+inputfile = input("Enter filename containing URIs to be checked: ")  
+f = open( inputfile, 'r')
+o = inputfile + '_results.txt'
+opf = open( o, 'w')
+count = 0
+while True:
+    count += 1
+    line = f.readline()
+    if not line:
+        break
+    url = line.strip()
+    try:
+        addr1 = socket.gethostbyname(url)
+        i.url_lookup(url)
+        i2.ip_lookup(addr1)
+        netobj = ipwhois.Net(addr1)
+        netasn = ipwhois.asn.IPASN(netobj)
+        netresult = netasn.lookup(netasn)
+        addrdomain = whois.query(url)
+        sys.stdout.write(u"{},{},{},{},{},{},{},{},{}\n".format(url, i.securityCategory, i.productivityCategory, addr1, i2.category, netresult['asn'], netresult['asn_description'], netresult['asn_cidr'], addrdomain.registrar))
+        sys.stdout.flush()
+        opf.write(u"{},{},{},{},{},{},{},{},{}}\n".format(url, i.securityCategory, i.productivityCategory, addr1, i2.category, netresult['asn'], netresult['asn_description'], netresult['asn_cidr'], addrdomain.registrar))
+        line = f.readline
+    except:
+        print( url, "is bad")
+        line = f.readline
+f.close
